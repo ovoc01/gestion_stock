b85a1c6029d26da32c73980dc36aee1b4fc294bf
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-09-05 11:43:00 +0300

    Modification du frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
-import "@/styles/globals.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- frontend/src/provider.tsx
+++
@@ -1,8 +0,0 @@
-import { NextUIProvider } from "@nextui-org/system";
-import { useNavigate } from "react-router-dom";
-
-export function Provider({ children }: { children: React.ReactNode }) {
-  const navigate = useNavigate();
-
-  return <NextUIProvider navigate={navigate}>{children}</NextUIProvider>;
-}
