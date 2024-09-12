a5586f68f705b5a31acbd7bac27dcc7f34f74d66
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-09-12 08:58:34 +0300

    backend: new spring starter

--- stockinventory/src/test/java/com/colasmadagascar/stockinventory/BackendApplicationTests.java
+++ stockinventory/src/test/java/com/colasmadagascar/stockinventory/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.company.colas.invetory.backend;
+package com.colasmadagascar.stockinventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
